fcd1e0e2df315ff51b042e1493bc2fb957848bbd
ohheebin <heebin@ohheebinui-MacBook-Pro.local>
2017-08-29 18:04:41 +0900

    news

--- news/day_crawling.py
+++ news/day_crawling.py
@@ -1,11 +1,29 @@
-#coding: utf-8
+
+# coding: utf-8
+
+# In[16]:
+
+
 import requests
 from bs4 import BeautifulSoup
-import datetime
 import re
 from konlpy.tag import Twitter
 import urllib.request
 import sys
+import pymysql
+
+
+# In[ ]:
+
+conn = pymysql.connect(host = 'localhost',
+                      user = 'root',
+                      password = '140508',
+                      db = 'news',
+                      charset = 'utf8')
+curs = conn.cursor()
+
+
+# In[10]:
 
 #크롤링함수 url의 본문 내용 저장
 def get_text(URL):
@@ -16,37 +34,48 @@ def get_text(URL):
         text = text + str(item.find_all(text=True))
     return text
 
+
+# In[11]:
+
 #클리닝함수 본문 내용 클리닝
 def clean_text(text):   
     cleaned_text = re.sub('[a-zA-Z]', '', text)
     cleaned_text = re.sub('[\{\}\[\]\/?.,;▶“’ⓒ:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]', '', cleaned_text)
     return cleaned_text
 
+
+# In[12]:
+
 # text저장 함수
 # url을 get_text(url)을 통해 기사의 내용을 가져온후 clean_text 함수를 통해 clean_text로 
 # 변경시켜 준후 명사화 시켜서 저장 
 def article_text(URL, num, day):
     spliter = Twitter()
+    #spliter = Mecab()
     try:
         OUTPUT_FILE_NAME = 'article_dict/'+ day +'/article_'+ day +'_'+ str(num) +'.txt'
         open_output_file = open(OUTPUT_FILE_NAME, 'w')
-        result_text = clean_text(get_text(URL))
-        article_nouns = spliter.nouns(result_text)
+        #result_text = clean_text(get_text(URL))
+        #article_nouns = spliter.nouns(result_text)
+        article_nouns = spliter.nouns(get_text(URL))
         for n in article_nouns: 
-            open_output_file.write(' ' + n)
+            if len(n) > 1 :
+                open_output_file.write(' ' + n)
         open_output_file.close()
     finally:
         open_output_file.close()
 
+
+# In[13]:
+
 #db에 저장하는 함수
-def db_manager(db_id, url, title, day):
-    """
-    db에 저장시킬 sql문 작성
-    명사화된 title저장
-    INSERT INTO db_article VALUES('db_id','url','title','nouns_title','day');
-    명사화된 title저장안하기
-    INSERT INTO db_article VALUES('db_id','url','title','day');
-    """
+def db_manager(url, title, day):
+    sql = "insert into data (url, title, date) values(%s,%s,%s)"
+    curs.execute(sql, (url, title, day))
+    conn.commit()
+
+
+# In[14]:
 
 #input ex) 20170808. 날짜 입력해서 crawling하기
 def main(day):
@@ -55,10 +84,6 @@ def main(day):
     #db에 저장 시킬 id
     db_id = 0
     first_url = ''
-    spliter = Twitter()
-    
-    #dict에 있는 article_text를 저장하기위해 만든 변수
-    num = 0
     
     while 1 == 1:
         url = 'http://news.naver.com/main/list.nhn?sid1=100&listType=title&mid=sec&mode=LSD&date='+str(day)+'&page='+str(page)
@@ -84,20 +109,21 @@ def main(day):
             if temp_page != page:
                 first_url = url
                 temp_page += 1
-            """
-            db에 저장시킬 함수 호출
-            db_manager(db_id, url, title, str(day))
-            print(url)
-            print(title)
-            print(nouns_title)
-            """
-            print(url)
-            #날짜 폴더가 생성되있어야 한다. 본문내용 저장 함수 호출
-            article_text(url,num,str(day))
             
-            num += 1
+            #db에 저장시킬 함수 호출
+            db_manager(url, title, str(day))
+            
+            print(url)
+            #print(title)
+            #날짜 폴더가 생성되있어야 한다. 본문내용 저장 함수 호출
+            article_text(url,db_id,str(day))
+            
             db_id += 1
         page += 1
 
+
+# In[ ]:
+
 if __name__ == '__main__':
     main(sys.argv[1])
+
