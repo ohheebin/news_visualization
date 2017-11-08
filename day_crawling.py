0d660f03b39c76e3d41acfeedb44522a106d9b8f
ohheebin <heebin@ohheebinui-MacBook-Pro.local>
2017-11-08 18:46:26 +0900

    news-visualization

---
+++ day_crawling.py
@@ -0,0 +1,118 @@
+
+# coding: utf-8
+
+# In[16]:
+
+
+import requests
+from bs4 import BeautifulSoup
+import re
+from konlpy.tag import Twitter
+import urllib.request
+import sys
+import pymysql
+
+
+# In[ ]:
+
+conn = pymysql.connect(host = 'localhost',
+                      user = 'root',
+                      password = '140508',
+                      db = 'news',
+                      charset = 'utf8')
+curs = conn.cursor()
+
+
+# In[10]:
+
+#크롤링함수 url의 본문 내용 저장
+def get_text(URL):
+    source_code_from_URL = urllib.request.urlopen(URL)
+    soup = BeautifulSoup(source_code_from_URL, 'lxml', from_encoding='utf-8')
+    text = ''
+    for item in soup.find_all('div', id='articleBodyContents'):
+        text = text + str(item.find_all(text=True))
+    return text
+
+
+# In[12]:
+
+# text저장 함수
+# url을 get_text(url)을 통해 기사의 내용을 가져온후 clean_text 함수를 통해 clean_text로 
+# 변경시켜 준후 명사화 시켜서 저장 
+def article_text(URL, num, day):
+    spliter = Twitter()
+    #spliter = Mecab()
+    try:
+        OUTPUT_FILE_NAME = 'article_dict/'+ day +'/article_'+ day +'_'+ str(num) +'.txt'
+        open_output_file = open(OUTPUT_FILE_NAME, 'w')
+        article_nouns = spliter.nouns(get_text(URL))
+        for n in article_nouns: 
+            if len(n) > 1 :
+                open_output_file.write(' ' + n)
+        open_output_file.close()
+    finally:
+        open_output_file.close()
+
+
+# In[13]:
+
+#db에 저장하는 함수
+def db_manager(url, title, day):
+    sql = "insert into data (url, title, date) values(%s,%s,%s)"
+    curs.execute(sql, (url, title, day))
+    conn.commit()
+
+
+# In[14]:
+
+#input ex) 20170808. 날짜 입력해서 crawling하기
+def main(day):
+    page = 1
+    temp_page = 0
+    #db에 저장 시킬 id
+    db_id = 0
+    first_url = ''
+    
+    while 1 == 1:
+        url = 'http://news.naver.com/main/list.nhn?sid1=100&listType=title&mid=sec&mode=LSD&date='+str(day)+'&page='+str(page)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text,'lxml')
+        
+        for body in soup.find_all("a", class_="nclicks(fls.list)"):
+             
+            #db에 저장 시킬 url
+            url = body.get('href')
+            
+            #마지막 페이지에 도달할시 함수 종료
+            if first_url == url:
+                return
+            
+            #db에 저장 시킬 article title
+            title = str(body.find_all(text=True))
+            #db에 저장 시킬 명사화된 article title 하지만 넣을지 말지 고민....
+            #nouns_title = clean_text(str(spliter.nouns(title)))
+            
+            #마지막 페이지를 구별하기 위해 만듬
+            if temp_page != page:
+                first_url = url
+                temp_page += 1
+            
+            #db에 저장시킬 함수 호출
+            db_manager(url, title, str(day))
+            
+            print(url)
+            #print(title)
+            #날짜 폴더가 생성되있어야 한다. 본문내용 저장 함수 호출
+            article_text(url,db_id,str(day))
+            
+            db_id += 1
+        page += 1
+
+
+# In[ ]:
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
---
+++ tfidf.py
@@ -0,0 +1,157 @@
+
+# coding: utf-8
+
+# In[1]:
+
+#전체 기사 내용에서 쓰이는 단어들의 tf-idf 값을 구해서 자주 쓰이는 쓸모없는 단어들을 수치로 표현해준다.
+import sys
+from konlpy.tag import Twitter
+from konlpy.tag import Mecab
+from collections import Counter
+import os
+import math
+import re
+
+
+# In[2]:
+
+#tfidf를 구하기 위해서 tf와 idf를 구하기 위한 시작 1글자 이상의 단어만
+# 생각해볼점 한글자인 것들을 제거 한후 할 것인지 아니면 1글자들도 tf-idf 를 구해서 제거해서 할 것인지
+# 제거 한 후 하는 것은 한 글자들은 대체로 의미가 없는 단어들이지만 필요한 단어가 있을 수도 있으며 좋은 점은 tf-idf 후 글자 clean의 시간을 단축시킴
+# 제거 하지 않으면 실제로 필요한 단어들도 존재 할 수 있으며 자주쓰이는 단어는 당연히 골라 낼 수 있을 것이다. 하지만 clean 시간이 길어 진다.
+def get_tags(text):
+    spliter = Twitter()
+    #spliter = Mecab()
+    nouns = spliter.nouns(text)
+    count = Counter(nouns)
+    return_list = []
+    for n, c in count.most_common():
+        if len(n) > 1:
+            temp = {'tag': n, 'count': c, 'dict_count' : 1}
+            return_list.append(temp)
+        #temp = {'tag': n, 'count': c, 'dict_count' : 1}
+        #return_list.append(temp)
+    return return_list
+
+
+# In[3]:
+
+#tfidf가 0에 가까울 수록 좋지 제거 대상
+#파라미터로는 각각의 단어에 대한 단어의 총 count와 어떤 문서에 쓰여져있는지의 count를 저장한 dict를 가져오고 총 문서의 수인 dict_num을 가져온다.
+#많은 문서에서 쓰이는 단어 일수록 idf 값이 0에 가까워 지며 그로 인해 tf-idf 값이 0에 가까워 지면 제거 대상이다.
+#로그빈도를 이용해서 tf값을 구했다. 값이 커지는 것을 막을 수 있다.
+def tfidf_func(result_dict, dict_num):
+    result = []
+    for tag in result_dict:
+        tf = math.log10(result_dict[tag][0] + 1)
+        idf = math.log10(dict_num / result_dict[tag][1])
+        tfidf = tf * idf
+        #print(tag,result_dict[tag][0],result_dict[tag][1],tfidf)
+        #tfidf 값을 조절해서 자주쓰이는 단어에 대해 알아낼수 있다.
+        if tfidf < 1.9 and result_dict[tag][0] > 10:
+            result.append(tag)
+            print(tag,result_dict[tag][0],result_dict[tag][1],tfidf)
+    #print(dict_num)
+    #print(result)
+    return result
+
+
+# In[20]:
+
+#tfidf 하기
+#처음에는 각각의 문서에 쓰여져 있는 단어의 count와 문서 count값을 가져와서 diction에 저장하고 계속해서 
+def result_tfidf():
+    num = 0
+    #전체 문서에 쓰인 명사와 그 명사의 count, 명사의 문서에 쓰인 count 수를 저장하기 위한 변수
+    result_dict = {}
+    output_file_name = 'noun.txt'
+    for (path, dir , files) in os.walk("article_dict"):
+        #존재 여부 확인
+        for fname in dir:
+            fullname = path+"/"+fname
+            #print(fullname)
+            for file in os.listdir(fullname):
+                #print(file)
+                if file.endswith(".txt"):
+                    open_text_file = open(fullname+"/"+file, 'r')
+                    text = open_text_file.read()
+            
+                    #get_tags(text)를 통해 하나의 문서에 쓰인 명사와 그 명사의 count수 그리고 문서 count수인 1을 return 받는다.
+                    tags = get_tags(text)
+                    open_text_file.close()
+            
+                    #전체적인 명사와 count, dict_count를 저장해 result_dict에 그 값들을 저장한다. 
+                    for tag in tags:
+                        noun = tag['tag']
+                        count = tag['count']
+                        dict_count = tag['dict_count']
+                        
+                        open_output_file = open(output_file_name, 'a+')
+                        open_output_file.write(noun+'\n')
+                        open_output_file.close()
+                        
+                        if noun in result_dict:
+                            result_dict[noun][0] += count
+                            result_dict[noun][1] += dict_count
+                        else:
+                            result_dict[noun] = [count, dict_count]
+                    
+                    #총 문서의 수를 저장하기 위해 사용하는 변수 하나의 문서를 처리할떄 마다 값을 1증가 시킨다.        
+                    num = num + 1
+    #위에서 계산한 result_dict와 전체 문서의 수인 num값을 파라미터로 넘겨준다.        
+    return tfidf_func(result_dict, num)  
+
+
+# In[21]:
+
+#tfidf를 통해 얻어온 제거 대상을 clean해주는 함수.
+def clean_text(text,remove_list):
+    cleaned_text = re.sub(remove_list, '', text)
+    return cleaned_text
+
+
+# In[22]:
+
+#main function 각 문서들의 최종 clean된 명사들을 word2vec하기위해 하나의 txt파일에 저장한다.
+def main():
+    spliter = Twitter()
+    remove_list = '|'.join(result_tfidf())
+    #입력 파일명 = file
+    for (path, dir , files) in os.walk("article_dict"):
+        for fname in dir:
+            fullname = path+"/"+fname
+            #print(fullname)
+            for file in os.listdir(fullname):
+                #존재 여부 확인
+                if file.endswith(".txt"):
+                    #출력 파일명
+                    output_file_name = 'result.txt'
+                    #output_file_name = 'konlpy/konlpy'+str(num)+'.txt'
+                    #print(fullname+"/"+file)
+                    open_text_file = open(fullname+'/'+file, 'r')
+                    text = open_text_file.read()
+                    open_text_file.close()
+            
+                    #open_output_file = open(output_file_name, 'w')
+                    open_output_file = open(output_file_name, 'a+')
+            
+                    result_text = clean_text(text,remove_list)
+                    nouns = spliter.nouns(result_text)
+                    
+                    for n in nouns:
+                        if len(n) > 1:
+                            open_output_file.write(' ' + n)
+                    open_output_file.write('\n')
+                    open_output_file.close()
+
+
+# In[23]:
+
+if __name__ == '__main__':
+    main()
+
+
+# In[ ]:
+
+
+
